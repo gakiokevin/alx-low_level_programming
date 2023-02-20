afdbc7dea75b1144f611ca172e99cd4d7f2c3286
gakiokevin <gakiokevin5@gmail.com>
2023-02-20 12:08:21 -0800

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point #Task to print alphabets in lowercase, except for e and q
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+	if (a != 'e' && a != 'q')
+	putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,5 @@ Printing different numbers
 Radndom value
 Alphabets
 Alphanumeric
+When I was having that alphabet soup, I never thought that it would pay off
+
