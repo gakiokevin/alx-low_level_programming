b842c85103de574dcd6103dcf99585e54ddaf303
gakiokevin <gakiokevin5@gmail.com>
2023-02-20 13:13:35 -0800

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include	<stdioh.h>
+/**
+* main - #Task: Print alphabets in lowercase in reverse(z-a)
+*
+* Return: Always 0 (Success)
+*/
+int	main(void)
+{
+	char	chr;
+
+	for	(chr	=	'z';	chr	>	'a';	chr--)
+	{
+		putchar(chr);
+		putchar('\n');
+	}
+	return	(0);
+}
