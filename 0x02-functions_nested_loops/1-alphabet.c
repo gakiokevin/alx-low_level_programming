84f2213a704d62f37dea574b6397799cbf25b995
gakiokevin <gakiokevin5@gmail.com>
2023-02-22 05:09:45 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,22 +3,10 @@
 /**
  * main - check the code
  * Return: Always 0.
+*print_alphabet()-afunctionthatprintsthealphabetinlowecase
  */
 int	main(void)
 {
 	print_alphabet();
 	return (0);
-}
-/**
-*print_alphabet()-afunctionthatprintsthealphabetinlowecase
-*/
-void print_alphabet(void)
-{
-	char	c;
-
-	for	(c	=	'a';	c	<=	'z';	c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
 }
