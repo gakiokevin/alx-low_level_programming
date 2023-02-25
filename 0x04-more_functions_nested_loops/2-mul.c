773455b9e47daa80e54064ab6c5594757c646702
gakiokevin <gakiokevin5@gmail.com>
2023-02-25 07:06:57 -0800

    2. Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,11 +3,13 @@
  * mul		-	a	function	that	multiply	'a'	and	'b'.
  *
  * Return:	a	+	b.
+ * @a:	first	value
+ * @b:	second	value
  */
 int mul(int a, int b)
 {
 	int	res;
 
-	res	=	a	+	b;
+	res	=	a	*	b;
 	return	(res);
 }
